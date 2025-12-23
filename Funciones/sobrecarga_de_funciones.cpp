14270c0bbd6b2316a60b4f97835f8f7afabb8bab
Adrian Pomareta Tarrago <adrianpomaretagit@gmail.com>
2025-12-23 12:09:00 +0100

    Curso C++ sobrecarga de funciones

---
+++ Funciones/sobrecarga_de_funciones.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Suma dos números enteros
+int sumar(int a, int b)
+{
+    return a + b;
+}
+
+// Suma tres números enteros
+int sumar(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+// Suma dos números decimales
+double sumar(double a, double b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << sumar(3, 5) << endl;        // Llama a sumar(int, int)
+    cout << sumar(2, 4, 6) << endl;     // Llama a sumar(int, int, int)
+    cout << sumar(2.5, 3.5) << endl;    // Llama a sumar(double, double)
+
+    return 0;
+}
